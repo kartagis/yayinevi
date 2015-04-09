2810aed6324fa28600fa49660d565845854b254a
git <tolga@ozses.net>
2015-04-09 17:29:59 +0300

    Commit

--- www/js/index.js
+++ www/js/index.js
@@ -47,14 +47,27 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+document.addEventListener('deviceready', function () {
+  if (navigator.notification) { // Override default HTML alert with native dialog
+    window.alert = function (message) {
+      navigator.notification.alert(
+        message,    // message
+        null,       // callback
+        "Workshop", // title
+        'OK'        // buttonName
+        );
+    };
+  }
+}, false);
+
 $("#search-btn").on("click", function() {
   $.ajax({
     url: "http://yayinevi.sabanciuniv.edu/",
-  type: 'GET',
-  dataType: 'json', // added data type
-  data: {"q": "search/node/"+$(".search").val().replace(/%2F/g, '/')},   // querystring params passed
-  success: function(data) {
-    alert(data);
-  }
+    type: 'GET',
+    dataType: 'json',
+    data: {"q": 'search/node/'+$("#search-txt").val()},
+    complete: function(data) {
+      alert(JSON.stringify(data));
+    }
   });
 })
