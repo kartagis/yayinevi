df62babf5bbf1d91cc167f0f4e074659a9d03d6e
git <tolga@ozses.net>
2015-04-16 21:31:14 +0300

    Commit

--- www/js/index.js
+++ www/js/index.js
@@ -16,61 +16,10 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-  // Application Constructor
-  initialize: function() {
-    this.bindEvents();
-  },
-  // Bind Event Listeners
-  //
-  // Bind any events that are required on startup. Common events are:
-  // 'load', 'deviceready', 'offline', and 'online'.
-  bindEvents: function() {
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-  },
-  // deviceready Event Handler
-  //
-  // The scope of 'this' is the event. In order to call the 'receivedEvent'
-  // function, we must explicitly call 'app.receivedEvent(...);'
-  onDeviceReady: function() {
-    app.receivedEvent('deviceready');
-  },
-  // Update DOM on a Received Event
-  receivedEvent: function(id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
-
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-
-    alert('Received Event: ' + id);
-  }
-};
-document.addEventListener("deviceready", onDeviceReady, false);
-// device APIs are available
-// //
-//
-function onDeviceReady() {
-  isOnline();
+$.ajax({
+  url: 'http://yayinevi.sabanciuniv.edu/content.json',
+dataType: 'json',
+success: function(data){
+  alert('Tamamlandı');
 }
-
-function isOnline() {
-  var networkState = navigator.connection.type;
-  var states = {};
-  states[Connection.UNKNOWN] = 'Unknown connection';
-  states[Connection.ETHERNET] = 'Ethernet connection';
-  states[Connection.WIFI] = 'WiFi connection';
-  states[Connection.CELL_2G] = 'Cell 2G connection';
-  states[Connection.CELL_3G] = 'Cell 3G connection';
-  states[Connection.CELL_4G] = 'Cell 4G connection';
-  states[Connection.CELL] = 'Cell generic connection';
-  states[Connection.NONE] = 'No network connection';
-  alert(states[networkState]);
-  /*
-     if ((states[networkState]) == states[Connection.NONE]) {
-     alert('No Internet Connection. Click OK to exit app');
-     navigator.app.exitApp();
-     }
-     */
-}
+});
