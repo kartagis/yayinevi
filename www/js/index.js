3319ec7cb3ab3d5d544e0c702de5fbe1d88b2d98
git <tolga@ozses.net>
2015-04-21 06:47:05 +0300

    Added the function

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,26 @@
-$.ajax({
-  url: 'http://yayinevi.sabanciuniv.edu/content.json',
-dataType: 'json',
-success: function (data) {
-  var title = data.nodes.map(function (node) {
-    return node.node.title;
+document.addEventListener('deviceready', createXHR, false);
+document.addEventListener('resume', createXHR, false);
+
+function createXHR() {
+  $.ajax({
+    url: 'http://yayinevi.sabanciuniv.edu/content.json',
+    dataType: 'json',
+    success: function (data) {
+      var title = data.nodes.map(function (node) {
+        return node.node.title;
+      });
+      var images = data.nodes.map(function (node) {
+        return node.node.field_resim.src;
+      });
+      alert(images);
+      /*
+         var template = "<img src='{{images}}'";
+         var html = Mustache.to_html(template, images);
+         $(".img").html(html);
+         */
+    },
+    error: function (data) {
+      alert('Bir hata oluştu');
+    }
   });
-  var images = data.nodes.map(function (node) {
-    return node.node.field_resim.src;
-  });
-  alert(images);
-  /*
-  var template = "<img src='{{images}}'";
-  var html = Mustache.to_html(template, images);
-  $(".img").html(html);
-  */
-},
-error: function (data) {
-  alert('Bir hata oluştu');
 }
-});
