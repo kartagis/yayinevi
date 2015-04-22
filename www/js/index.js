43f4e4ca640b4fc576fdf4afe2d686b6c0de0fc2
git <tolga@ozses.net>
2015-04-22 17:04:18 +0300

    Commit

--- www/js/index.js
+++ www/js/index.js
@@ -6,20 +6,13 @@ function createXHR() {
     url: 'http://yayinevi.sabanciuniv.edu/content.json',
     dataType: 'json',
     success: function (data) {
-      var images = data.nodes.map(function (node) {
-        return {image: node.node.field_resim.src};
+      $.each(content.nodes, function (key, value) {
+        var d=document;
+        var img = d.createElement('img');
+        img.className = 'img';
+        img.src = value.node.field_resim.src;
+        d.body.appendChild(img);
       });
-      var title = data.nodes.map(function (node) {
-        return node.node.title;
-      });
-      var translator=data.nodes.map(function(node) {
-        return node.node.field_ceviri;
-      });
-      var stuff = {images: images};
-      var images='{{#images}}<img src="{{image}}">{{/images}}';
-      var titles='{{#images}}<img src="{{image}}">{{/images}}';
-      var html=Mustache.to_html(images, stuff);
-      $("div").html(html);
     },
     error: function (data) {
       alert('Bir hata oluştu');
