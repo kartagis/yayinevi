1ab9b51088ca59daeaf282d52dc19738662b8eb1
git <tolga@ozses.net>
2015-04-29 22:42:24 +0300

    inappbrowser plugin installed, lightbox js installed, files adjusted

--- www/js/index.js
+++ www/js/index.js
@@ -1,39 +1,49 @@
 document.addEventListener('DOMContentLoaded', createXHR, false);
-document.addEventListener('resume', createXHR, false);
+//document.addEventListener('resume', createXHR, false);
 
 function createXHR() {
+  var buy;
   $.ajax({
     url: 'http://yayinevi.sabanciuniv.edu/content.json',
     dataType: 'json',
     success: function (data) {
+      function createElement(eleType, className) {
+        var ele = document.createElement(eleType);
+        ele.className = className;
+        return ele;
+      }
       $.each(data.nodes, function (key, value) {
-        var d = document;
-        var img = d.createElement('img');
-        img.className = 'img';
-        img.src = value.node.field_resim.src;
-        var title = d.createElement('div');
-        title.className = 'title';
-        title.innerHTML = value.node.title;
-        var price = d.createElement('div');
-        price.className = 'price';
-        price.innerHTML = value.node.field_fiyat_1;
-        var buy = d.createElement('div');
-        buy.innerHTML = value.node.field_satin_al;
-        var btn = d.createElement('input');
-        btn.type = 'button';
-        btn.value = 'Buy';
-        var hr = d.createElement('hr');
-        btn.className = 'btn success';
-        d.body.appendChild(title);
-        d.body.appendChild(img);
-        d.body.appendChild(price);
-        d.body.appendChild(btn);
-        d.body.appendChild(hr);
-        $('.success').on('click', function() {
-          $(this).blur();
-          window.location = 'http://yayinevi.sabanciuniv.edu/index.php?do=catalog/payment&amp;lang=tr&amp;id='+buyinnerHTML+'&amp;externalId=";"';
-        });
+        var d = document,
+      a = createElement('a', 'img'),
+      img = createElement('img', 'img'),
+      title = createElement('div', 'title'),
+      price = createElement('div', 'price'),
+      buy = createElement('div'),
+      btn = createElement('input', 'btn success'),
+      row = createElement('div', 'row'),
+      clear = createElement('div', 'clear');
+      a.dataset.title = value.node.title;
+      a.setAttribute('href', value.node.field_resim.src);
+      a.dataset.lightbox = 'lightbox';
+      d.body.appendChild(a);
+      a.appendChild(img);
+      img.src = value.node.field_resim.src;
+      title.innerHTML = value.node.title;
+      price.innerHTML = value.node.field_fiyat_1;
+      buy.innerHTML = value.node.field_satin_al;
+      btn.type = 'button';
+      btn.value = price.innerHTML + ' TL';
+      row.appendChild(title);
+      row.appendChild(btn);            
+      row.appendChild(clear);
+      d.body.appendChild(row);
       });
     }
   });
 }
+$(document).on('click', '.success', function () {
+  var supay=window.open('https://supay.sabanciuniv.edu/index.php?do=catalog/payment&lang=tr&id=' + buy.innerHTML, '_blank', 'location=yes');
+  supay.addEventListener('loadstart', function() {
+    alert('Start');
+  });
+});
