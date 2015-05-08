425d7daa7872ad93acc2856697adfb9b2d56ed33
git <tolga@ozses.net>
2015-05-08 12:32:54 +0300

    Almost working

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,44 @@
-//document.addEventListener('DOMContentLoaded', createXHR, false);
-document.addEventListener('deviceready', createXHR, false);
-document.addEventListener('resume', createXHR, false);
+document.addEventListener('DOMContentLoaded', createXHR, false);
 
 function createXHR() {
   var buy;
   $.ajax({
-    url: 'http://yayinevi.sabanciuniv.edu/content.json',
-    dataType: 'json',
-    success: function (data) {
-      function createElement(eleType, className) {
-        var ele = document.createElement(eleType);
-        ele.className = className;
-        return ele;
-      }
-      $.each(data.nodes, function (key, value) {
-        var d = document,
-            a = createElement('a', 'img'),
-            img = createElement('img', 'img'),
-            title = createElement('div', 'title'),
-            price = createElement('div', 'price'),
-            buy = createElement('div'),
-            btn = createElement('input', 'btn success'),
-            row = createElement('div', 'row'),
-            clear = createElement('div', 'clear');
-      a.dataset.title = value.node.title;
-      a.setAttribute('href', value.node.field_resim.src);
-      a.dataset.lightbox = 'lightbox';
-      d.body.appendChild(a);
-      a.appendChild(img);
-      img.src = value.node.field_resim.src;
-      title.innerHTML = value.node.title;
-      price.innerHTML = value.node.field_fiyat_1;
-      buy.innerHTML = value.node.field_satin_al;
-      btn.type = 'button';
-      btn.value = price.innerHTML + ' TL';
-      row.appendChild(title);
-      row.appendChild(btn);            
-      row.appendChild(clear);
-      d.body.appendChild(row);
-      });
+      url: 'http://yayinevi.sabanciuniv.edu/content.json',
+      dataType: 'json',
+      success: function(data) {
+        function createElement(eleType, className) {
+          var ele = document.createElement(eleType);
+          ele.className = className;
+          return ele;
+        }
+        $.each(data.nodes, function(key, value) {
+            var d = document,
+                a = createElement('a', 'img'),
+                img = createElement('img', 'img'),
+                title = createElement('div', 'title'),
+                price = createElement('div', 'price'),
+                btn = createElement('input', 'btn success'),
+                row = createElement('div', 'row'),
+                clear = createElement('div', 'clear');
+            a.dataset.title = value.node.title;
+            a.setAttribute('href', value.node.field_resim.src);
+            a.dataset.lightbox = 'lightbox';
+            d.body.appendChild(a);
+            a.appendChild(img);
+            img.src = value.node.field_resim.src;
+            title.innerHTML = value.node.title;
+            price = value.node.field_fiyat_1;
+            buy = value.node.field_satin_al;
+            btn.type = 'button';
+            btn.value = price + ' TL';
+            row.appendChild(title);
+            row.appendChild(btn);
+            row.appendChild(clear);
+            d.body.appendChild(row);
+            $(document).live('click', '.success', function() {
+                console.log("window.open('https://supay.sabanciuniv.edu/index.php?do=catalog/payment&lang=tr&id=' + buy, '_blank', 'location=yes'"));
+            });
+        });
     }
   });
 }
-$(document).on('click', '.success', function () {
-  //window.open = cordova.InAppBrowser.open;
-  var supay = cordova.InAppBrowser.open('https://supay.sabanciuniv.edu/index.php?do=catalog/payment&lang=tr&id=' + buy.innerHTML, '_blank', 'location=yes');
-  supay.addEventListener('loadstart', function() {
-    alert('Start');
-  });
-});
