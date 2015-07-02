515ba4da4eb9ea7490a3ceab83194e4f3a0691df
git <tolga@ozses.net>
2015-07-02 06:45:49 +0300

    Added deviceready and resume events

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', createXHR, false);
+document.addEventListener('deviceready', createXHR, false);
+document.addEventListener('resume', createXHR, false);
 
 function createXHR() {
   var buy;
@@ -37,10 +38,10 @@ function createXHR() {
             row.appendChild(clear);
             d.body.appendChild(row);
             });
-	$(document).on('click', '.success', function (ev) {
-			var buy = $(ev.target).attr("data-buy");
-			window.open('https://supay.sabanciuniv.edu/index.php?do=catalog/payment&lang=tr&id=' + buy);
-			});
+            $(document).on('click', '.success', function (ev) {
+                var buy = $(ev.target).attr("data-buy");
+                inappbrowser.open('https://supay.sabanciuniv.edu/index.php?do=catalog/payment&lang=tr&id=' + buy);
+            });
       }
   });
 }
